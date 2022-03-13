461b52ed9a418d7ee1b01ff7255b5b3aac5c5de2
Sebastian Andica <53380760+Sebastian1811@users.noreply.github.com>
2022-03-13 12:41:09 -0500

    Update database.ts

--- src/database.ts
+++ src/database.ts
@@ -2,7 +2,7 @@ import { connect } from "mongoose";
 
 export async function startConn() {
   // Cambiar la ruta y el puerto
-  await connect("mongodb://172.21.134.164:32054/clientsdb", {
+  await connect("mongodb://8009aff0-us-south.lb.appdomain.cloud:27017/clientsdb", {
     useNewUrlParser: true,
     useFindAndModify: false,
   });
